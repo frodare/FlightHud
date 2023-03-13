9915450ca948c8d1bac8ede255d1ea5c1929855f
Oblio <oblio.leitch@gmail.com>
2023-03-13 13:55:30 -0400

    fixes for 1.19.3

--- src/main/java/net/torocraft/flighthud/components/PitchIndicator.java
+++ src/main/java/net/torocraft/flighthud/components/PitchIndicator.java
@@ -2,142 +2,143 @@ package net.torocraft.flighthud.components;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3f;
+import net.minecraft.util.math.RotationAxis;
 import net.torocraft.flighthud.Dimensions;
 import net.torocraft.flighthud.FlightComputer;
 import net.torocraft.flighthud.HudComponent;
 
 public class PitchIndicator extends HudComponent {
-  private final Dimensions dim;
-  private final FlightComputer computer;
-  private final PitchIndicatorData pitchData = new PitchIndicatorData();
+    private final Dimensions dim;
+    private final FlightComputer computer;
+    private final PitchIndicatorData pitchData = new PitchIndicatorData();
 
-  public PitchIndicator(FlightComputer computer, Dimensions dim) {
-    this.computer = computer;
-    this.dim = dim;
-  }
-
-  @Override
-  public void render(MatrixStack m, float partial, MinecraftClient mc) {
-    pitchData.update(dim);
-
-    float horizonOffset = computer.pitch * dim.degreesPerPixel;
-    float yHorizon = dim.yMid + horizonOffset;
-
-    float a = dim.yMid;
-    float b = dim.xMid;
-
-    float roll = computer.roll * (CONFIG.pitchLadder_reverseRoll ? -1 : 1);
-
-    if (CONFIG.pitchLadder_showRoll) {
-      m.push();
-      m.translate(b, a, 0);
-      m.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(roll));
-      m.translate(-b, -a, 0);
+    public PitchIndicator(FlightComputer computer, Dimensions dim) {
+        this.computer = computer;
+        this.dim = dim;
     }
 
-    if (CONFIG.pitchLadder_showLadder) {
-      drawLadder(mc, m, yHorizon);
+    @Override
+    public void render(MatrixStack m, float partial, MinecraftClient mc) {
+        pitchData.update(dim);
+
+        float horizonOffset = computer.pitch * dim.degreesPerPixel;
+        float yHorizon = dim.yMid + horizonOffset;
+
+        float a = dim.yMid;
+        float b = dim.xMid;
+
+        float roll = computer.roll * (CONFIG.pitchLadder_reverseRoll ? -1 : 1);
+
+        if (CONFIG.pitchLadder_showRoll) {
+            m.push();
+            m.translate(b, a, 0);
+            m.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(roll));
+            //m.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(roll));
+            m.translate(-b, -a, 0);
+        }
+
+        if (CONFIG.pitchLadder_showLadder) {
+            drawLadder(mc, m, yHorizon);
+        }
+
+        drawReferenceMark(mc, m, yHorizon, CONFIG.pitchLadder_optimumClimbAngle);
+        drawReferenceMark(mc, m, yHorizon, CONFIG.pitchLadder_optimumGlideAngle);
+
+        if (CONFIG.pitchLadder_showHorizon) {
+            pitchData.l1 -= pitchData.margin;
+            pitchData.r2 += pitchData.margin;
+            drawDegreeBar(mc, m, 0, yHorizon);
+        }
+
+        if (CONFIG.pitchLadder_showRoll) {
+            m.pop();
+        }
     }
 
-    drawReferenceMark(mc, m, yHorizon, CONFIG.pitchLadder_optimumClimbAngle);
-    drawReferenceMark(mc, m, yHorizon, CONFIG.pitchLadder_optimumGlideAngle);
+    private void drawLadder(MinecraftClient mc, MatrixStack m, float yHorizon) {
+        int degreesPerBar = CONFIG.pitchLadder_degreesPerBar;
 
-    if (CONFIG.pitchLadder_showHorizon) {
-      pitchData.l1 -= pitchData.margin;
-      pitchData.r2 += pitchData.margin;
-      drawDegreeBar(mc, m, 0, yHorizon);
+        if (degreesPerBar < 1) {
+            degreesPerBar = 20;
+        }
+
+        for (int i = degreesPerBar; i <= 90; i = i + degreesPerBar) {
+            float offset = dim.degreesPerPixel * i;
+            drawDegreeBar(mc, m, -i, yHorizon + offset);
+            drawDegreeBar(mc, m, i, yHorizon - offset);
+        }
+
     }
 
-    if (CONFIG.pitchLadder_showRoll) {
-      m.pop();
-    }
-  }
+    private void drawReferenceMark(MinecraftClient mc, MatrixStack m, float yHorizon, float degrees) {
+        if (degrees == 0) {
+            return;
+        }
 
-  private void drawLadder(MinecraftClient mc, MatrixStack m, float yHorizon) {
-    int degreesPerBar = CONFIG.pitchLadder_degreesPerBar;
+        float y = (-degrees * dim.degreesPerPixel) + yHorizon;
 
-    if (degreesPerBar < 1) {
-      degreesPerBar = 20;
-    }
+        if (y < dim.tFrame || y > dim.bFrame) {
+            return;
+        }
 
-    for (int i = degreesPerBar; i <= 90; i = i + degreesPerBar) {
-      float offset = dim.degreesPerPixel * i;
-      drawDegreeBar(mc, m, -i, yHorizon + offset);
-      drawDegreeBar(mc, m, i, yHorizon - offset);
-    }
+        float width = (pitchData.l2 - pitchData.l1) * 0.45f;
+        float l1 = pitchData.l2 - width;
+        float r2 = pitchData.r1 + width;
 
-  }
-
-  private void drawReferenceMark(MinecraftClient mc, MatrixStack m, float yHorizon, float degrees) {
-    if (degrees == 0) {
-      return;
+        drawHorizontalLineDashed(m, l1, pitchData.l2, y, 3);
+        drawHorizontalLineDashed(m, pitchData.r1, r2, y, 3);
     }
 
-    float y = (-degrees * dim.degreesPerPixel) + yHorizon;
+    private void drawDegreeBar(MinecraftClient mc, MatrixStack m, float degree, float y) {
 
-    if (y < dim.tFrame || y > dim.bFrame) {
-      return;
+        if (y < dim.tFrame || y > dim.bFrame) {
+            return;
+        }
+
+        int dashes = degree < 0 ? 4 : 1;
+
+        drawHorizontalLineDashed(m, pitchData.l1, pitchData.l2, y, dashes);
+        drawHorizontalLineDashed(m, pitchData.r1, pitchData.r2, y, dashes);
+
+        int sideTickHeight = degree >= 0 ? 5 : -5;
+        drawVerticalLine(m, pitchData.l1, y, y + sideTickHeight);
+        drawVerticalLine(m, pitchData.r2, y, y + sideTickHeight);
+
+        int fontVerticalOffset = degree >= 0 ? 0 : 6;
+
+        drawFont(mc, m, String.format("%d", i(Math.abs(degree))), pitchData.r2 + 6,
+                (float) y - fontVerticalOffset);
+
+        drawFont(mc, m, String.format("%d", i(Math.abs(degree))), pitchData.l1 - 17,
+                (float) y - fontVerticalOffset);
     }
 
-    float width = (pitchData.l2 - pitchData.l1) * 0.45f;
-    float l1 = pitchData.l2 - width;
-    float r2 = pitchData.r1 + width;
+    private static class PitchIndicatorData {
+        public float width;
+        public float mid;
+        public float margin;
+        public float sideWidth;
+        public float l1;
+        public float l2;
+        public float r1;
+        public float r2;
 
-    drawHorizontalLineDashed(m, l1, pitchData.l2, y, 3);
-    drawHorizontalLineDashed(m, pitchData.r1, r2, y, 3);
-  }
+        public void update(Dimensions dim) {
+            width = i(dim.wScreen / 3);
+            float left = width;
 
-  private void drawDegreeBar(MinecraftClient mc, MatrixStack m, float degree, float y) {
+            mid = i((width / 2) + left);
+            margin = i(width * 0.3d);
+            l1 = left + margin;
+            l2 = mid - 7;
+            sideWidth = l2 - l1;
+            r1 = mid + 8;
+            r2 = r1 + sideWidth;
+        }
 
-    if (y < dim.tFrame || y > dim.bFrame) {
-      return;
+        private int i(double d) {
+            return (int) Math.round(d);
+        }
     }
-
-    int dashes = degree < 0 ? 4 : 1;
-
-    drawHorizontalLineDashed(m, pitchData.l1, pitchData.l2, y, dashes);
-    drawHorizontalLineDashed(m, pitchData.r1, pitchData.r2, y, dashes);
-
-    int sideTickHeight = degree >= 0 ? 5 : -5;
-    drawVerticalLine(m, pitchData.l1, y, y + sideTickHeight);
-    drawVerticalLine(m, pitchData.r2, y, y + sideTickHeight);
-
-    int fontVerticalOffset = degree >= 0 ? 0 : 6;
-
-    drawFont(mc, m, String.format("%d", i(Math.abs(degree))), pitchData.r2 + 6,
-        (float) y - fontVerticalOffset);
-
-    drawFont(mc, m, String.format("%d", i(Math.abs(degree))), pitchData.l1 - 17,
-        (float) y - fontVerticalOffset);
-  }
-
-  private static class PitchIndicatorData {
-    public float width;
-    public float mid;
-    public float margin;
-    public float sideWidth;
-    public float l1;
-    public float l2;
-    public float r1;
-    public float r2;
-
-    public void update(Dimensions dim) {
-      width = i(dim.wScreen / 3);
-      float left = width;
-
-      mid = i((width / 2) + left);
-      margin = i(width * 0.3d);
-      l1 = left + margin;
-      l2 = mid - 7;
-      sideWidth = l2 - l1;
-      r1 = mid + 8;
-      r2 = r1 + sideWidth;
-    }
-
-    private int i(double d) {
-      return (int) Math.round(d);
-    }
-  }
 
 }
